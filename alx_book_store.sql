38baa61dd399b96a5bbe878ddc8dbba68c62da08
codewithshadyy <kipkoechshadrack10@gmail.com>
2025-12-10 12:12:04 +0000

    intro to db

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -2,13 +2,13 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE AUTHORS (
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT,
     author_name VARCHAR(215) NOT NULL
 );
 
 
-CREATE TABLE BOOKS (
+CREATE TABLE Books (
     book_id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(130) NOT NULL,
     author_id INT,
@@ -18,7 +18,7 @@ CREATE TABLE BOOKS (
 );
 
 
-CREATE TABLE CUSTOMERS (
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
@@ -26,7 +26,7 @@ CREATE TABLE CUSTOMERS (
 );
 
 
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_id INT,
     order_date DATE,
@@ -34,7 +34,7 @@ CREATE TABLE ORDERS (
 );
 
 
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
